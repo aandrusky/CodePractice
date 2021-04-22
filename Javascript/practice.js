2e791c4323c4123f09f3ef9dd101d37c901f58fb
Alan Andrusky <alanandrusky@gmail.com>
2021-04-22 11:08:08 -0500

    new challenge complete

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -182,4 +182,26 @@ function distanceBetweenPoints(a, b) {
   return Math.hypot(a.x - b.x, a.y - b.y);
 }
 
+//9)
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
+
+const abbrevName = (name) => {
+
+  const nameArray = name.split(" ");
+
+  const letterGetter = nameArray.map(n => n[0]).join(".")
+
+  return letterGetter
+}
+
+const test = abbrevName("Jaron Lane")
+
+console.log(test)
