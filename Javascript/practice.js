fe3b309733b615d7a6f2460f88fe271287bb03c0
Alan Andrusky <alanandrusky@gmail.com>
2021-04-27 10:32:55 -0500

    default value practice

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -246,3 +246,33 @@ function firstRepeating(str)
 // Driver code
 let str = "gaass";
 console.log(firstRepeating(str));
+
+//11)
+
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+const howManyLightsabersDoYouOwn = (name = "") => {
+  if (name == "Zach") {
+    result = 18
+  } 
+
+  else {
+    result = 0
+  }
+
+  return result
+}
+
+const test = howManyLightsabersDoYouOwn("Dave")
+console.log(test)
